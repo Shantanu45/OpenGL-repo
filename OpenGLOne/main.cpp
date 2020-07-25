66f5d0483bda858e4fa588e7dbb79072c12efaa0
Shantanu45 <30995267+Shantanu45@users.noreply.github.com>
2020-07-25 20:06:11 +0530

    Moved Directional Light to its own class which is subclass of Light

--- OpenGLOne/main.cpp
+++ OpenGLOne/main.cpp
@@ -18,7 +18,7 @@
 #include "Shader.h"
 #include "Camera.h"
 #include "Texture.h"
-#include "Light.h"
+#include "DirectionalLight.h"
 #include "Material.h"
 
 const float toRadians = 3.14159265f / 180.0f;
@@ -34,7 +34,7 @@ Texture dirtTexture;
 Material shinyMaterial;
 Material dullMaterial;
 
-Light mainLight;
+DirectionalLight mainLight;
 
 GLfloat deltaTime = 0.0f;
 GLfloat lastTime = 0.0f;
@@ -126,8 +126,9 @@ int main()
 	shinyMaterial = Material(1.0f, 32);
 	dullMaterial = Material(0.3f, 4);
 
-	mainLight = Light(1.0f, 1.0f, 1.0f, 0.1f,
-		0.0f, 0.0f, -1.0f, 0.3f);
+	mainLight = DirectionalLight(1.0f, 1.0f, 1.0f, 
+								0.1f, 0.3f,
+								0.0f, 0.0f, -1.0f);
 
 	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
 		uniformAmbientIntensity = 0, uniformAmbientColour = 0, uniformDirection = 0, uniformDiffuseIntensity = 0,
