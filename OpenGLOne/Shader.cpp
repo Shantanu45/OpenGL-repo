5054d8cafe41c70bcf0e602c3a2af9de5e7afe71
Shantanu45 <30995267+Shantanu45@users.noreply.github.com>
2020-07-25 12:39:32 +0530

    added diffuse lighting

--- OpenGLOne/Shader.cpp
+++ OpenGLOne/Shader.cpp
@@ -82,7 +82,8 @@ void Shader::CompileShader(const char* vertexCode, const char* fragmentCode)
 	uniformView = glGetUniformLocation(shaderID, "view");
 	uniformAmbientColour = glGetUniformLocation(shaderID, "directionalLight.colour");
 	uniformAmbientIntensity = glGetUniformLocation(shaderID, "directionalLight.ambientIntensity");
-
+	uniformDirection = glGetUniformLocation(shaderID, "directionalLight.direction");
+	uniformDiffuseIntensity = glGetUniformLocation(shaderID, "directionalLight.diffuseIntensity");
 }
 
 GLuint Shader::GetProjectionLocation()
@@ -93,20 +94,25 @@ GLuint Shader::GetModelLocation()
 {
 	return uniformModel;
 }
-
 GLuint Shader::GetViewLocation()
 {
 	return uniformView;
 }
-
 GLuint Shader::GetAmbientColourLocation()
 {
 	return uniformAmbientColour;
 }
-
 GLuint Shader::GetAmbientIntensityLocation()
 {
 	return uniformAmbientIntensity;
+}
+GLuint Shader::GetDiffuseIntensityLocation()
+{
+	return uniformDiffuseIntensity;
+}
+GLuint Shader::GetDirectionLocation()
+{
+	return uniformDirection;
 }
 
 void Shader::UseShader()
